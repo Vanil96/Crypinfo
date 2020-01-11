be232af0fb677a6b8173828af82a775a9ec4f67b
Vanil96 <vanildm@gmail.com>
2020-01-11 02:40:54 +0100

    progress

--- javascript/globalinfo.js
+++ javascript/globalinfo.js
@@ -10,9 +10,11 @@ let btc_dominacja = glob[0].btc_d;  //w procentach
 let volume = glob[0].total_volume;     // wolumen z 24h
 
 //document.getElementById('ilosc_coinow').textContent= ilosc_coinow;
-document.getElementById('kapitalizacja').textContent= kapitalizacja.toLocaleString(); 
-document.getElementById('btc_dominacja').textContent = btc_dominacja; 
-document.getElementById('volume').textContent= volume.toLocaleString(); 
+document.getElementById('kapitalizacja').textContent= kapitalizacja.toLocaleString()+" $"; 
+document.getElementById('btc_dominacja').textContent = btc_dominacja+"%"; 
+document.getElementById('volume').textContent= volume.toLocaleString()+" $"; 
+
+document.getElementById('progress_bar').setAttribute("style", "width:"+btc_dominacja+"%");
 }
 
 globalinfo();
